a0f0817438556fed15601f0c1a9ae6db0e525d0f
Abel Laurente <abellaurente@gmail.com>
2026-06-02 20:54:53 +0000

    cuarto

--- src/controlador/ctrl_mf.js
+++ src/controlador/ctrl_mf.js
@@ -19,9 +19,47 @@ export const programas=async (req,res)=>{
     }
 }
 
+
+export const por_programa=async (req,res)=>{
+    try{
+        const [rows]=await pool.query("select nom_prd, nom_act, nom_sub, unidad, format(sum(meta_fisica),0) as meta from metas_fisicas where cod_prg=? group by nom_prg, nom_prd, nom_act, nom_sub, unidad order by nom_prd, nom_act, nom_sub",[req.params.prg]);
+        res.json(rows);
+    }catch(error){
+        return res.status(500).json({mensaje:'ocurrio un error'});
+    }
+}
+
+export const productos=async (req,res)=>{
+    try{
+        const [rows]=await pool.query("select distinct(cod_prd) as cod_prd, nom_prd from metas_fisicas where cod_prg=?",[req.params.prg]);
+        res.json(rows);
+    }catch(error){
+        return res.status(500).json({mensaje:'ocurrio un error'});
+    }
+}
+
+export const por_producto=async (req,res)=>{
+    try{
+        const [rows]=await pool.query("select nom_act, nom_sub, unidad, format(sum(meta_fisica),0) as meta from metas_fisicas where cod_prg=? and cod_prd=? group by nom_prg, nom_prd, nom_act, nom_sub, unidad order by nom_prd, nom_act, nom_sub", [req.params.prg, req.params.prd])
+        res.json(rows);
+    }catch(error){
+        return res.status(500).json({mensaje:'ocurrio un error'});
+    }
+}
+
+export const por_programa_micro=async (req,res)=>{
+    try{
+        const [rows]=await pool.query("select mf.nom_prd, mf.nom_act, mf.nom_sub, mr.nom_micro, mf.unidad, format(sum(mf.meta_fisica),0) as meta from metas_fisicas mf, micro_red mr, eess est where mf.cod_eess=est.cod_eess and est.cod_micro = mr.cod_micro and mf.cod_prg=? group by mf.nom_prd, mf.nom_act, mf.nom_sub, mr.nom_micro, mf.unidad order by mf.nom_prd, mf.nom_act, mf.nom_sub, meta desc", [req.params.prg])
+        res.json(rows);
+    }catch(error){
+        return res.status(500).json({mensaje:'ocurrio un error'});
+    }
+}
+
+
 export const get_prod=async (req,res)=>{
     try{
-        const [rows]=await pool.query('select distinct(cod_prd), nom_prd from ktx where cod_prg = ?',[req.params.prg]);
+        const [rows]=await pool.query('select distinct(cod_prd), nom_prd from ktx where cod_prg = ?',[req.params.prg, req.params.prd]);
         res.json(rows);
     }catch(error){
         return res.status(500).json({mensaje:'ocurrio un error'});
--- src/rutas/rutas_mf.js
+++ src/rutas/rutas_mf.js
@@ -1,10 +1,14 @@
 import {Router} from 'express';
-import {get_ini, get_prod, kit_programa, programas} from '../controlador/ctrl_mf.js';
+import {get_ini, get_prod, kit_programa, por_producto, por_programa, por_programa_micro, productos, programas} from '../controlador/ctrl_mf.js';
 const router=Router();
 
 router.get('/inicio',get_ini);
 router.get('/programas',programas);
-router.get('/productos/:prg',get_prod);
+router.get('/por_programa/:prg',por_programa);
+router.get('/productos/:prg',productos);
+router.get('/por_producto/:prg/:prd',por_producto);
+router.get('/por_programa_micro/:prg',por_programa_micro);
+
 router.get('/actividades/:prg/:prd',get_prod);
 router.get('/kit_programa/:prg',kit_programa);
 
